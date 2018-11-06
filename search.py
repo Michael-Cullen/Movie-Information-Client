817c5e18cf4a8f37162fef40affe641851162108
Luke McCartney <26875439+lukemccartney@users.noreply.github.com>
2018-11-06 14:43:29 +0000

    GUI Upload
    GUI created with the relevant libraries, search function implemented but data is returned to the console still.

--- search.py
+++ search.py
@@ -1,11 +1,48 @@
+import sys
 import omdb
-from sys import stdin
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-omdb.set_default('apikey', '582f52b9')
+class App(QMainWindow):
+    omdb.set_default('apikey', '582f52b9')
+    def __init__(self):
+        super().__init__()
+        self.title = 'Movie Information Client'
+        self.left = 10
+        self.top = 10
+        self.width = 400
+        self.height = 140
+        self.initUI()
 
-def search_function():
-    t = input("Enter film name:")
-    res = omdb.search(t)
-    print(res)
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-search_function()
+        # Create textbox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280,40)
+
+        # Create a button in the window
+        self.button = QPushButton('Show text', self)
+        self.button.move(20,80)
+
+        # connect button to function on_click
+        self.button.clicked.connect(self.on_click)
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        textboxValue = self.textbox.text()
+        QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+        self.textbox.setText("")
+        res = omdb.search(textboxValue)
+        for r in res:
+            print(r["title"],"-",r["year"])
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
